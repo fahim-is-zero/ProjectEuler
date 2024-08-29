bc028380a4c0e3b1866238f1d32be3b19369ab25
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-29 22:35:53 +0600

    added comments

--- Python/problem-001.py
+++ Python/problem-001.py
@@ -4,7 +4,7 @@ def sum_multi(num):
 	numbers = []
 	
 	for i in range(1, num):
-		if (i % 3 == 0) or (i % 5 == 0):
+		if (i % 3 == 0) or (i % 5 == 0):  # Checks multiplication of 3 or 5
 			numbers.append(i)
 		else:
 			pass
