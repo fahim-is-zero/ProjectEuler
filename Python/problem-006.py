a116701a4c2c0d3a797517c62da18b0563b08e81
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-29 22:33:33 +0600

    added comment

--- Python/problem-006.py
+++ Python/problem-006.py
@@ -1,11 +1,11 @@
 # Difference between the sum of the squares of the first 100 natural numbers and the square of the sum.
 
 def sum_square(limit):
-    num = list(range(1, limit + 1))
+    num = list(range(1, limit + 1))  # same as ( n(n+1)/2 )^2
     return (sum(num))**2
 
 def square_sum(limit):
-    return ( (limit*(limit + 1)*(2 * limit + 1)) / 6 )
+    return ( (limit*(limit + 1)*(2 * limit + 1)) / 6 )  # Formula : n(n+1)(2n+1)/6
 
 def diff_series(limit):
     result = sum_square(limit) - square_sum(limit)
