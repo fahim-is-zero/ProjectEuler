c5fbd3a34134de636b6a9a5342b0bbff9249b422
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-30 10:39:42 +0600

    added comments

--- Python/problem-004.py
+++ Python/problem-004.py
@@ -1,11 +1,11 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 def largest_palindrome(limit):
-    largest_found = 0                               
+    largest_found = 0                               # Stores largest found palindrome from all combination
     for i in range(limit, 0, -1):
         for j in range(i, 0, -1):
             product = i * j
-            if str(product) == str(product)[::-1]:  
+            if str(product) == str(product)[::-1]:  # Check if the product is a palindrome
                     if product > largest_found:
                         largest_found = product
     return largest_found
