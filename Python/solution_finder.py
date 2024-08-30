c0238d6cb88f638ece831a6a96e13f0cbf149f99
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-30 10:43:03 +0600

    Add files via upload

---
+++ Python/solution_finder.py
@@ -0,0 +1,13 @@
+# Finds the solutions to project euler using keys
+
+solutions = {"001" : 233168,
+             "002" : 4613732,
+             "006" : 25164150             
+}
+
+
+def solution_finder(key):
+    return solutions[key]
+
+print("Input the number of the problem in XXX format (e.g. : 017 )")
+print(solution_finder(input("Number of the problem : ")))
