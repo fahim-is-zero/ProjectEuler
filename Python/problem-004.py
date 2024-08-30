26add2bf45afa3380d03279a988d51171c3fe3c9
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-30 12:09:27 +0600

    added comments

--- Python/problem-004.py
+++ Python/problem-004.py
@@ -3,7 +3,7 @@
 def largest_palindrome(limit):
     largest_found = 0                               # Stores largest found palindrome from all combination
     for i in range(limit, 0, -1):
-        for j in range(i, 0, -1):
+        for j in range(i, 0, -1):  # efficient than range(limit, 0, -1)
             product = i * j
             if str(product) == str(product)[::-1]:  # Check if the product is a palindrome
                     if product > largest_found:
