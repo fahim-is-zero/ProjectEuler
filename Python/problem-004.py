3a720ee11eac34a84f1456a9c166a26a6cfee2c2
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-30 10:39:15 +0600

    Create problem-004.py

---
+++ Python/problem-004.py
@@ -0,0 +1,13 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def largest_palindrome(limit):
+    largest_found = 0                               
+    for i in range(limit, 0, -1):
+        for j in range(i, 0, -1):
+            product = i * j
+            if str(product) == str(product)[::-1]:  
+                    if product > largest_found:
+                        largest_found = product
+    return largest_found
+
+print(largest_palindrome(999))  # 906609
