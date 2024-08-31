b09d7b3070cebbd6629d355347f8c6f22f0af7d4
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-08-31 10:52:20 +0600

    added comments

--- Python/problem-016.py
+++ Python/problem-016.py
@@ -4,7 +4,7 @@ def power_digit_sum(power):
     result = (2 ** power)
     sum = 0
     for i in str(result):
-        sum += int(i)
+        sum += int(i)     # add digits
     print(sum)
 
 power_digit_sum(1000) # 1366
