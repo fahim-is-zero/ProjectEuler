2a09a8cfaf94afa21ec62e2baef7aee823a46c16
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-05 11:15:09 +0600

    Update problem-048.py

--- Python/problem-048.py
+++ Python/problem-048.py
@@ -3,11 +3,10 @@ def self_power(limit):
     n = 1
 
     for i in range(limit):
-        total += (n+i)**(n+i)
+        total += (n+i)**(n+i)  # series : [n^n + (n+1)^(n+1) + (n+2)^(n+2) + .... (n+i)^(n+i)]
 
         str_total =str(total)
-        last_ten_digits = str_total[-10:]
-
+        last_ten_digits = str_total[-10:] 
     print(last_ten_digits)
 
-print(self_power(1000))
+print(self_power(1000)) 
