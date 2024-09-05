80196b4c40c28bfd5deb261bfbb77f10adf23589
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-05 11:15:26 +0600

    added comments

--- Python/problem-048.py
+++ Python/problem-048.py
@@ -1,3 +1,5 @@
+# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + .... + 1000^1000  (n^n + ...)
+
 def self_power(limit):
     total = 0
     n = 1
@@ -6,7 +8,8 @@ def self_power(limit):
         total += (n+i)**(n+i)  # series : [n^n + (n+1)^(n+1) + (n+2)^(n+2) + .... (n+i)^(n+i)]
 
         str_total =str(total)
-        last_ten_digits = str_total[-10:] 
+        last_ten_digits = str_total[-10:]  # Stores last ten digits
+
     print(last_ten_digits)
 
-print(self_power(1000)) 
+print(self_power(1000)) # 9110846700
