9f10ef26daaa3e3648601cec81539991e432cc69
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-08 06:20:26 +0600

    added comments

--- Python/problem-025.py
+++ Python/problem-025.py
@@ -1,5 +1,7 @@
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
 def fibo(n):
+    """returns n-th fibonacci sequence"""
     a, b = 0, 1
     for _ in range(n -1):
         a, b = b, a + b
@@ -7,8 +9,8 @@ def fibo(n):
 
 def fibo_digits_idx(digits):
     idx = 1
-    while not len(str(fibo(idx))) == digits:
+    while not len(str(fibo(idx))) == digits:  # checks the lenth of fibonacci num
         idx += 1
     return idx
 
-print(fibo_digits_idx(1000))
+print(fibo_digits_idx(1000))  # 4782
