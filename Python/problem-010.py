c30ef246e08b1252a3afa30f104909b2c434858c
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-14 00:53:29 +0600

    added comments and improved code

--- Python/problem-010.py
+++ Python/problem-010.py
@@ -1,19 +1,22 @@
-from math import floor
+# Find the sum of all the primes below two million.
 
 
-def is_prime(n):
+from math import floor, sqrt
+
+
+def is_prime(n):  # explaination is on project euler website
     if n == 1:
         return False
-    elif n < 4:
+    elif n < 4:  # 2 and 3
         return True
     elif n % 2 == 0:
         return False
-    elif n < 9:
+    elif n < 9:  # already excluded 4,6,8
         return True
     elif n % 3 == 0:
         return False
     else:
-        r = floor(n**(0.5))
+        r = floor(sqrt(n))
         f = 5
         while f <= r:
             if n % f == 0:
@@ -23,8 +26,8 @@ def is_prime(n):
             f = f + 6
         return True
 
-def sum_prime(limit): 
-    sum = 5 
+def sum_prime(limit):  # explaination is on project euler website
+    sum = 5  # as 2 and 3 are prime
     num = 5 
     while num <= limit:
         if is_prime(num):
@@ -36,4 +39,4 @@ def sum_prime(limit):
     return sum  
 
 
-print(sum_prime(2000000))
+print(sum_prime(2000000)) # 142913828922
